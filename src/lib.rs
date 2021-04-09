677238fa475a6bbf93c068b742951b19ee7bdd3c
g <guidomaurocurcio@gmail.com>
2021-04-09 15:28:23 -0300

    use mutex in inner, use VecDeque instead of Vec;

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,54 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Condvar, Mutex},
+};
+
+// The deque wraps around, an implementation with modulo pops up in mind, where we update how the deque wraps
+// around cyclically while removing elements from it.
+// Contrast this with using a vec where you need to resize and fill the holes of a removed object.
 
 pub struct Sender<T> {
-    inner: Arc<Mutex<Inner<T>>>,
+    inner: Arc<Inner<T>>,
+}
+
+impl<T> Sender<T> {
+    pub fn send(&mut self, t: T) {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.push_back(t);
+        drop(queue);
+        self.inner.available.notify_one();
+    }
 }
 pub struct Receiver<T> {
-    inner: Arc<Mutex<Inner<T>>>,
+    inner: Arc<Inner<T>>,
+}
+
+impl<T> Receiver<T> {
+    pub fn recv(&mut self) -> T {
+        let mut queue = self.inner.queue.lock().unwrap();
+        loop {
+            match queue.pop_front() {
+                Some(t) => return t,
+                None => {
+                    queue = self.inner.available.wait(queue).unwrap();
+                }
+            }
+        }
+    }
 }
 
 struct Inner<T> {
     // Holds the data that is shared. The things on the channel.
-    queue: Vec<T>,
+    queue: Mutex<VecDeque<T>>,
+    available: Condvar,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let inner = Inner { queue: Vec::new() };
-    let inner = Arc::new(Mutex::new(inner));
+    let inner = Inner {
+        queue: Mutex::default(),
+        available: Condvar::new(),
+    };
+    let inner = Arc::new(inner);
     (
         Sender {
             inner: inner.clone(),
